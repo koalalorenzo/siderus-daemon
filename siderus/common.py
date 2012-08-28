205bf0c7df8ba549c0441ade83a3aa045f4bc6f6
Lorenzo Setale <koalalorenzo@gmail.com>
2012-08-28 20:40:41 +0200

    Implemented some new common functions

--- siderus/common.py
+++ siderus/common.py
@@ -15,26 +15,30 @@ def return_myaddr():
 		socket.setdefaulttimeout(None)
 	return addr
 
-def return_localip():
+def return_subnet_localip():
 	""" This function returns the local IP """
 	sockcon = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	sockcon.connect(('www.google.com',80))
 	ip, localport = sockcon.getsockname()
 	sockcon.close()
 	return ip
+	
 
-def get_random_port():
+def get_random_port(exclude_list=None):
 	""" This function returns a random port between 49152 and 65535 """
 	random.seed()
-	port = random.randint(49152, 65535) 
-	return port
+	port = random.randint(49152, 65535)
+	if exclude_list:
+		if int(port) in exclude_list: return get_random_port()
+	if int(port) is 52125: return get_random_port()
+	return int(port)
 	
 def from_addr_to_dict(address):
 	"""
 		This function read an address and transform it in a dictionary:
 		
-		>>> from_addr_to_dict("application@10.0.0.22:14916")
-		{ "addr": "10.0.0.2", "port": 14916, "app": "application" } 
+		>>> from_addr_to_dict("application@10.0.0.22:52125")
+		{ "addr": "10.0.0.2", "port": 52125, "app": "application" } 
 		
 	"""
 	dict_addr = dict()
@@ -49,7 +53,29 @@ def from_dict_to_addr(dict_addr):
 		This function read a dictionary and transform it in an address.
 
 		The dict_addr MUST be filled like this:
-		{ "addr": "10.0.0.2", "port": 14916, "app": "application" }
+		{ "addr": "10.0.0.2", "port": 52125, "app": "application" }
 	"""
 	return "%s:%s:%s" % ( dict_addr['app'], dict_addr['addr'], dict_addr['port'] )
 
+def is_local_address(raw_address):
+	""" This function returns True if the address is pointing to localhost """
+	addr_dict = from_addr_to_dict(raw_address)
+	if "localhost" in addr_dict: return Ture
+	if "127.0.0.1" in addr_dict: return Ture
+	return False
+	
+def is_addressed_to_daemon(raw_address):
+	""" This function check if the raw_address is pointing to a daemon"""
+	addr_dict = from_addr_to_dict(raw_address)
+	if addr_dict["app"] == "daemon": return True
+	return False
+	
+def return_daemon_address(ip_address):
+	""" This function returns the raw address of a specific daemon """
+	
+	dict_addr = dict()
+	dict_addr["app"] = "daemon"
+	dict_addr["addr"] = ip_address
+	dict_addr["port"] = 52125
+	
+	return from_dict_to_addr(dict_addr)
