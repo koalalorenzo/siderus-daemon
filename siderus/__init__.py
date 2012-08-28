5c5f51f32cba41b29914a907e82afc4586de7c9f
Lorenzo Setale <koalalorenzo@gmail.com>
2012-08-28 08:36:20 +0200

    Added the old Siderus module, and improved gitignore

---
+++ siderus/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding=utf-8 -*-
+
+"""Siderus P2P Connecting System"""
+
+__version__ = "0.4"
+__author__ = "Lorenzo Setale ( http://www.setale.me/ )"
+__license__ = "See: http://creativecommons.org/licenses/by-nd/3.0/ "
+__copyright__ = "Copyright (c) 2009, 2010, 2011, 2012, 2013 Lorenzo Setale"
