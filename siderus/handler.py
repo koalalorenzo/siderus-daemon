3304146e7db1b4d775d44a6ba696c65d65e706eb
Lorenzo Setale <koalalorenzo@gmail.com>
2012-08-31 10:56:53 +0200

    Implemented some functions to analyze the message received

--- siderus/handler.py
+++ siderus/handler.py
@@ -21,19 +21,52 @@ class DaemonHandler(object):
 	"""
 	def __init__(self):
 		self.connections = list()
-		self.message_cache = list()
-		self.applications = list()
+		self.messages_cache = list() #TODO: load it from database
+		self.applications_ports = dict() # { 'app': 123 }
+		
+	def __analyze_message_from_local_app(self, message):
+		return
+		
+	def __analyze_message_from_remote_app(self, message):
+		return
+		
+	def __forward_message(self, received_message):
+		destination_dict = from_addr_to_dict(message.destination)
+
+		if destination_dict['app'] in self.applications.keys():
+			destination_dict['port'] = int(self.applications[destination_dict['app']])
+		else:
+			# If app "installed" but not active: cache the forwarded message!
+			return
+		
+		destination_dict['addr'] = "127.0.0.1"
+		new_destination = from_dict_to_addr(destination_dict)
+		forward = Message()
+		forward.content = received_message.content
+		forward.origin = received_message.origin
+		forward.destination = new_destination
+		
+		#if app is listening then send. Otherwise cache the message.
+		forward.send()
 		
 	def analyze(self, message):
 		""" This function decode and analyze the message. """
 		message.decode()
+
+		if message.is_corrupted():
+			#reply with a new "error" message with the sent message.__hash.
+			return
+
 		destination = from_addr_to_dict(message.destination)
 		if destination['app'] == "daemon":
-			if is_local_address(message.destination):
-				#do applications and connections stuff
+			if is_local_address(message.destination): 
+				self.__analyze_message_from_local_app(message)
+				return
 			else:
-				#accept connections
+				self.__analyze_message_from_remote_app(message)
+				return
 		else:
+			print
 			#forward message to the application
 		return
 		
@@ -44,4 +77,10 @@ class DaemonHandler(object):
 			message.receive()
 			#thread this process:
 			self.analyze(message)
+			
+			
+	def clear_cache(self):
+		""" This function sends all the message saved in the cache. """:
+		for message in self.messages_cache:
+			message.send()
 	
